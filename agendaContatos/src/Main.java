1048918dcf0763ca4177137752535dff4f2bfac3
Rafael Lopes <rafalsilva.ti@gmail.com>
2025-06-08 11:01:55 -0300

    Update Main.java

--- agendaContatos/src/Main.java
+++ agendaContatos/src/Main.java
@@ -12,24 +12,6 @@ public class Main {
     ArvoreBinaria arvore = new ArvoreBinaria();
     ContatoControle controle = new ContatoControle();
 
-    /*List<Contato> contatos = new ArrayList<>();
-
-      //  arvore.exibirPreOrdem(arvore.getRaiz());
-        
-        new InterfaceUsuario(controle, arvore);
-    arvore.inserirContato(new Contato("Rafael", "(63) 12345-6789", "teste@email.com"));
-    arvore.inserirContato(new Contato("Pedro", "(63) 98765-4321", "teste2@email.com"));
-    arvore.inserirContato(new Contato("Sabrina", "(63) 11111-2222", "teste3@email.com"));
-
-    arvore.exibirPreOrdem(arvore.getRaiz(), contatos);
-
-    for (Contato c : contatos) {
-      System.out.println(c.getNome() + " | " + c.getTelefone() + " | " + c.getEmail());
-    }*/
-
      new InterfaceUsuario(controle, arvore);
-
-    // System.out.println(arvore.buscarPorNome("Rafael"));
-
   }
 }
